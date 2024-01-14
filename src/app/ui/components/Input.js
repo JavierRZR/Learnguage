0f968b8341ee7e46c95c451479348288ae0fe07c
Javi <ruizromero98@gmail.com>
2024-01-14 02:04:42 +0100

    Fix minor issues on Input.js and add filters URL modification.

--- src/app/ui/components/Input.js
+++ src/app/ui/components/Input.js
@@ -1,7 +1,7 @@
+"use client"
 
 
-
-export default function Input({ id, name, type, children }) {
+export default function Input({ id, name, type, handleChange, children }) {
 
 
     return (
@@ -11,10 +11,12 @@ export default function Input({ id, name, type, children }) {
                 {name}
             </label>
             <input id={id} type={type} placeholder="#MPA Mamutardo"
-                className="p-2 rounded-md bg-stone-300 text-neutral-800 outline
+                className="p-2 rounded-md bg-stone-300 text-neutral-800 
+                outline outline-neutral-300
                  hover:outline-neutral-300/80 
                  focus:outline-emerald-300/50 focus:outline-2
-                 transition-all" />
+                 transition-all"
+                onChange={(e) => handleChange(e.target.value)} />
             {children}
         </div>
     )
